c83795485b64f291dfdbed5012d78a26cd4ac236
TamIT001 <phanminhtam247@gmail.com>
2017-10-11 17:16:10 +0700

    add showed posted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
 
   skip_before_action :require_login, only: [:create, :index]
-  def index;end
+  def index
+    @articles = Article.all
+  end
   
   def dashboard
+    @articles = Article.where(account_id: session[:current_user_id])
   end
 
   def new
@@ -17,10 +20,10 @@ class ArticlesController < ApplicationController
     @article.title = @param[:title]
     @article.text = @param[:text]
     @article.account = Account.find(session[:current_user_id])
-    if @param[:picture]
-      upload()
-    end
     if @article.save
+      if @param[:picture]
+        upload(@article.id)
+      end
       redirect_to dashboard_url
     else
       render :new
@@ -28,12 +31,17 @@ class ArticlesController < ApplicationController
     #render json: @article
     #render plain: params[:article].inspect
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
 
   private
 
-  def upload
+  def upload(id)
     uploaded_io = new_post_params()[:picture]
-    File.open(Rails.root.join('app', 'assets/images/post','main.png'), 'wb') do |file|
+    Dir.mkdir(Rails.root.join('public', 'uploads', "#{id}"))
+    File.open(Rails.root.join('public', 'uploads', "#{id}", 'main.jpg'), 'wb') do |file|
       file.write(uploaded_io.read)
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   match 'accounts/login', to: 'accounts#login', via: [:post]
   match '/logout', to: 'accounts#logout', via: [:get]
   match '/dashboard', to: 'articles#dashboard', via: [:get]
+  match '/articles/search', to: 'articles#search', via: [:get]
   #match 'accounts/index', to: 'accounts#index', via: [:get]
   root 'articles#index'
 end
